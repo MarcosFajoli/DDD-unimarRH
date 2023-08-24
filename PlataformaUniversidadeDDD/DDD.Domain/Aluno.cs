c6a9b0ec0bf1ad50edd0ca03139f306db30dc17e
navarrounimar <navarrounimar@gmail.com>
2023-08-23 21:56:47 -0300

    Required em Nome Aluno

--- PlataformaUniversidadeDDD/DDD.Domain/Aluno.cs
+++ PlataformaUniversidadeDDD/DDD.Domain/Aluno.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace DDD.Domain
     public class Aluno
     {
         public int Id { get; set; }
+        [Required]
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
 
