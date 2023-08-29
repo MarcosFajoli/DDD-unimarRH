f3e1dfd9e7c02e3f637e403d6601a58d85a5851d
navarrounimar <navarrounimar@gmail.com>
2023-08-28 22:59:15 -0300

    classe SqlServer

---
+++ PlataformaUniversidadeDDD/DDD.Infra.SQLServer/SqlContext.cs
@@ -0,0 +1,20 @@
+﻿using DDD.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DDD.Infra.SQLServer
+{
+    public class SqlContext : DbContext
+    {
+
+        //https://balta.io/blog/ef-crud
+        //https://jasonwatmore.com/post/2022/03/18/net-6-connect-to-sql-server-with-entity-framework-core
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost,1233; Initial Catalog=UniversidadeDb; User Id=user; Password=test123");
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Disciplina> Disciplinas { get; set; }
+    }
+}
