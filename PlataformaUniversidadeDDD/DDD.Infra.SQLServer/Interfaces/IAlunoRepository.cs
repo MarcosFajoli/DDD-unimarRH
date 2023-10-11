7389f5c34a8074651eb242b8d91d46ec2c64ea91
Marcos Fajoli <markosalmeidaa@gmail.com>
2023-10-10 22:15:16 -0300

    true: fix merge

---
+++ PlataformaUniversidadeDDD/DDD.Infra.SQLServer/Interfaces/IAlunoRepository.cs
@@ -0,0 +1,18 @@
+﻿using DDD.Domain.SecretariaContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Infra.SQLServer.Interfaces
+{
+    public interface IAlunoRepository
+    {
+        public List<Aluno> GetAlunos();
+        public Aluno GetAlunoById(int id);
+        public void InsertAluno(Aluno aluno);
+        public void UpdateAluno(Aluno aluno);
+        public void DeleteAluno(Aluno aluno);
+    }
+}
---
+++ PlataformaUniversidadeDDD/DDD.Infra.SQLServer/Repositories/AlunoRepositorySqlServer.cs
@@ -0,0 +1,77 @@
+﻿using DDD.Domain.SecretariaContext;
+using DDD.Infra.SQLServer.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Infra.SQLServer.Repositories
+{
+    public class AlunoRepositorySqlServer : IAlunoRepository
+    {
+
+        private readonly SqlContext _context;
+
+        public AlunoRepositorySqlServer(SqlContext context)
+        {
+            _context = context;
+        }
+
+        public void DeleteAluno(Aluno aluno)
+        {
+            try
+            {
+                _context.Set<Aluno>().Remove(aluno);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public Aluno GetAlunoById(int id)
+        {
+            return _context.Alunos.Find(id);
+        }
+
+        public List<Aluno> GetAlunos()
+        {
+            //return  _context.Alunos.Include(x => x.Disciplinas).ToList();
+            return _context.Alunos.ToList();
+
+        }
+
+        public void InsertAluno(Aluno aluno)
+        {
+            try
+            {
+                _context.Alunos.Add(aluno);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                //log exception
+
+            }
+        }
+
+        public void UpdateAluno(Aluno aluno)
+        {
+            try
+            {
+                _context.Entry(aluno).State = EntityState.Modified;
+                _context.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
